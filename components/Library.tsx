f96c8475c2de2efeb5c2c5efec3743639ac0a1f3
aisankheiri <aisankheiri@marun.edu.tr>
2023-08-22 11:19:16 +0300

    add authentication for + button

--- components/Library.tsx
+++ components/Library.tsx
@@ -2,11 +2,24 @@
 
 import {TbPlaylist} from "react-icons/tb";
 import {AiOutlinePlus} from "react-icons/ai";
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+import useUploadModal from "@/hooks/useUploadModal";
 
 
 const Library = () => {
+
+    const authModal = useAuthModal();
+    const uploadModal = useUploadModal();
+    const { user } = useUser();
+
     const onClick =()=>{
-        //Handle Upload Later
+       if(!user){
+        return authModal.onOpen();
+       }
+       //TODO : check for subscription
+
+       return uploadModal.onOpen();
     };
   return (
     <div className="flex flex-col">
