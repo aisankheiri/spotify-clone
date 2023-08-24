d11fbbfc1945a5fc982e89a55ef44ca4c0c2e717
aisankheiri <aisankheiri@marun.edu.tr>
2023-08-24 11:07:21 +0300

    Add the music player and its buttons

--- app/(site)/components/PageContent.tsx
+++ app/(site)/components/PageContent.tsx
@@ -1,13 +1,19 @@
 "use client";
 
 import { Song } from "@/types";
-import SongItem from "./SongItem";
+import SongItem from "../../../components/SongItem";
+import useOnPlay from "@/hooks/useOnPlay";
 
 interface PageContentProbs {
   songs: Song[];
 }
 
-const PageContent: React.FC<PageContentProbs> = ({ songs }) => {
+const PageContent: React.FC<PageContentProbs> = ({
+   songs
+   }) => {
+
+    const onPlay = useOnPlay(songs);
+    
   if (songs.length === 0) {
     return <div className="mt-4 text-neutral-400">No songs available</div>;
   }
@@ -27,7 +33,9 @@ const PageContent: React.FC<PageContentProbs> = ({ songs }) => {
         "
     >
       {songs.map((item) => (
-        <SongItem key={item.id} onClick={() => {}} data={item} />
+        <SongItem key={item.id} 
+        onClick={(id:string) => onPlay(id)} 
+        data={item} />
       ))}
     </div>
   );
