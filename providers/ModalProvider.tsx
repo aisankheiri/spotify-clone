dcb923cdb8633cf3ef2d5b2710ea3f60a5d33ed9
aisankheiri <aisankheiri@marun.edu.tr>
2023-08-26 12:51:53 +0300

    add rules when someone just subscribe can play the musics

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -1,12 +1,18 @@
 "use client";
 
 import AuthModal from "@/components/AuthModal";
+import SubscribeModal from "@/components/SubscribeModal";
 import UploadModal from "@/components/UploadModal";
+import { ProductWithPrice } from "@/types";
 import { useEffect, useState } from "react";
 
+interface ModalProviderProbs{
+  products : ProductWithPrice[];
+}
 
-
-const ModalProvider = () => {
+const ModalProvider:React.FC<ModalProviderProbs> = ({
+  products
+}) => {
     const [isMounted , setIsMounted] = useState(false);
 
 
@@ -23,6 +29,8 @@ const ModalProvider = () => {
     <>
      <AuthModal/>
      <UploadModal/>
+     <SubscribeModal products={products}/>
+   
     </>
   )
 }
